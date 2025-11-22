69794ae375906a1bb205c28000581ee67fb8ee0c
Ankit patel <smartstudyankit@gmail.com>
2025-11-22 15:24:07 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  apiKey: "AIzaSyCepk3b0Gy_A75uWRXocCw0Vs-yvWbywgQ",
+  authDomain: "successwithankit-f6544.firebaseapp.com",
+  projectId: "successwithankit-f6544",
+  storageBucket: "successwithankit-f6544.firebasestorage.app",
+  messagingSenderId: "350768343882",
+  appId: "1:350768343882:web:2600985334acdaefe12fb3",
+  measurementId: "G-LW5C099MB0"
+};
